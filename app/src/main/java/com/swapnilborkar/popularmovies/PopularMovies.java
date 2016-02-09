00d003c87f30edb400c50d2dc5316adac1c0c092
Swapnil Borkar <swapnilborkar@live.com>
2016-02-09 23:26:33 +0530

    - Preparing for CustomArrayAdapter wit PopularMovies class

---
+++ app/src/main/java/com/swapnilborkar/popularmovies/PopularMovies.java
@@ -0,0 +1,16 @@
+package com.swapnilborkar.popularmovies;
+
+
+public class PopularMovies
+{
+    String movieName;
+    String movieSummary;
+    int moviePoster; //drawable reference id
+
+
+    public PopularMovies(String movieName, String movieSummary, int moviePoster) {
+        this.movieName = movieName;
+        this.movieSummary = movieSummary;
+        this.moviePoster = moviePoster;
+    }
+}
